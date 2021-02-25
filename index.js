be4a5a9e3136304fbf251ca83730579913f0aafc
yusardv <yusardv@pop-os.localdomain>
2021-02-25 10:03:57 +0300

    H

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-var map = L.map('mapid').setView([41.015137, 28.979530], 13);
+var map = L.map('mapid').setView([41.015137, 28.979530], 7);
 
 L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token=pk.eyJ1IjoibWFwYm94IiwiYSI6ImNpejY4NXVycTA2emYycXBndHRqcmZ3N3gifQ.rJcFIG214AriISLbB6B5aw', {
 		maxZoom: 18,
@@ -19,13 +19,14 @@ function onMapClick(e) {
 
 map.on('click', onMapClick);
 
-L.Routing.control({
+var control = L.Routing.control({
     waypoints: [
         L.latLng(41.168834, 28.923225),
         L.latLng(41.054243, 29.236336)
     ],
     routeWhileDragging: true,
     geocoder: L.Control.Geocoder.nominatim(),
+    profile: "foot",
     lineOptions: {
         styles: [{color: 'red', opacity: 0.5, weight: 5}]
     },
@@ -36,6 +37,11 @@ L.Routing.control({
             {color: 'blue', opacity: 0.5, weight: 2}
         ]
     }
-}).addTo(map);
+})
 
-console.log($(".leaflet-routing-geocoder")[0].text)
+control.addTo(map);
+i = 0
+
+
+
+   
